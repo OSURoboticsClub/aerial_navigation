c74668ea075f8f70a061fb380613dbac977af5f1
ScottJordan <Scott.M.Jordan91@gmail.com>
2014-12-20 17:16:46 -0800

    TestCommit for Trying out the image files

---
+++ TestImage_Detect/Debug/subdir.mk
@@ -0,0 +1,25 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../Main.cpp 
+
+OBJS += \
+./Main.o 
+
+CPP_DEPS += \
+./Main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: NVCC Compiler'
+	/usr/local/cuda-6.5/bin/nvcc -G -g -O0 -gencode arch=compute_30,code=sm_30 -gencode arch=compute_32,code=sm_32  -odir "" -M -o "$(@:%.o=%.d)" "$<"
+	/usr/local/cuda-6.5/bin/nvcc -G -g -O0 --compile  -x c++ -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ TestImage_Detect/Release/subdir.mk
@@ -0,0 +1,25 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../Main.cpp 
+
+OBJS += \
+./Main.o 
+
+CPP_DEPS += \
+./Main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: NVCC Compiler'
+	/usr/local/cuda-6.5/bin/nvcc -O3 -ccbin /usr/bin/arm-linux-gnueabihf-g++-4.8 -gencode arch=compute_30,code=sm_30 -gencode arch=compute_32,code=sm_32  -odir "" -M -o "$(@:%.o=%.d)" "$<"
+	/usr/local/cuda-6.5/bin/nvcc -O3 -ccbin /usr/bin/arm-linux-gnueabihf-g++-4.8 --compile  -x c++ -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
