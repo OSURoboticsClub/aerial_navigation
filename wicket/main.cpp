7cf6f6de8958a53a928ad9b2f97c7456273fa676
Matthew Lei <lei.matthew93@gmail.com>
2015-03-19 08:30:05 +0000

    thorough commenting

--- wicket/main.cpp
+++ wicket/main.cpp
@@ -1,3 +1,12 @@
+/*
+ * Creator: Matthew Lei
+ * Last updated: march 19 2015
+ * Description:
+ * main file for reading an image/video or capturing video feed and applying
+ * various transformation filters to achieve wanted effect. Controlling what
+ * filters to apply should be simply just uncommenting defines from the control
+ * center below.
+ */
 #include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -14,15 +23,20 @@ using namespace std;
 using namespace cv;
 using namespace cv::gpu;
 
+
+/** control center **/
 #define DEBUG
-//#define CALC_FPS
+#define CALC_FPS
 
 #define RES_480
 //#define SHOW_RAW
-#define APPLY_HSV_FILTER
 #define APPLY_GAUSSIAN_BLUR
+//#define APPLY_HSV_FILTER
+//#define APPLY_BOUNDING_BOX
 #define APPLY_CANNY_EDGE
-#define APPLY_HOUGH_LINE
+//#define APPLY_HOUGH_LIN
+E
+/********************/
 
 #ifdef RES_480
 const int cam_height = 480;
@@ -48,6 +62,11 @@ string inputFile;
 string trackbarWindow;
 string windowName;
 
+
+/*
+ * function to open a VideoCapture with given video filename
+ * **unfinished, barely used**
+ */
 void getVideoFromFile(std::string filename, VideoCapture dest)
 {
 	if (dest.open(filename) == false) {
@@ -60,6 +79,11 @@ void getVideoFromFile(std::string filename, VideoCapture dest)
 	}
 }
 
+
+/*
+ * function to set up camera settings before operation
+ * **Currently not working. Setting width and height doesn't work**
+ */
 void cameraSetup(VideoCapture &capture)
 {
 	//params.filterByColor = true;
@@ -75,13 +99,7 @@ void cameraSetup(VideoCapture &capture)
 	cout << "Height: " << capture.get(CV_CAP_PROP_FRAME_HEIGHT) << endl;
 }
 
-void wicketOverlay(Mat frame, Point p1, Point p2)
-{
-	
-}
-
-
-
+//useless function. Broken code upon deletion?? weirdest bug I have ever encountered.
 Mat applyOpening(Mat src)
 {
 	Mat dst;
@@ -94,12 +112,21 @@ Mat applyOpening(Mat src)
 	//not quite sure how this function works yet
 	int operation = morph_operator + 2;
 	Mat element = getStructuringElement( morph_elem, Size( 2*morph_size + 1, 2*morph_size+1 ), Point( morph_size, morph_size ) );
-
 	//apply opening morphology
-	morphologyEx(src, dst, operation, element);
+	//morphologyEx(src, dst, operation, element);
 	return dst;
 }
 
+
+/*
+ * function to filter/mask orange colors from frame
+ * input: 3 channel rgb Mat frame
+ * output: 3 channel rgb Mat frame with orange filter mask applied
+ * description:
+ *  - converts the original frame in HSV color format
+ *  - applies hard-coded thresholds to each channel
+ *  - masks original image with threshold mask
+ */
 Mat filterOrange(Mat frame)
 {
 	GpuMat gpuFrame(frame);
@@ -114,11 +141,11 @@ Mat filterOrange(Mat frame)
 	hsv_split[1].copyTo(sat);
 	hsv_split[2].copyTo(val);
 
-	int min_hue = 70;
-	int max_hue = 130;
-	int min_sat = 70;
+	int min_hue = 100;
+	int max_hue = 120;
+	int min_sat = 100;
 	int max_sat = 255;
-	int min_val = 150;
+	int min_val = 40;
 	int max_val = 255;
 
 	//apply threshold to each channel
@@ -136,14 +163,17 @@ Mat filterOrange(Mat frame)
 	gpu::bitwise_and(hsv_split[0], hsv_split[1], binaries);
 	gpu::bitwise_and(hsv_split[2], binaries, binaries);
 
+	//mask original image with binary
 	GpuMat finalGpu;
 	gpuFrame.copyTo(finalGpu, binaries);
+	//binaries.copyTo(finalGpu);
 
 	Mat final;
 	finalGpu.download(final);
 	return final;
 }
 
+//debugging function to show frame on separate test window.
 void show(Mat frame)
 {
 	namedWindow("test", WINDOW_NORMAL);
@@ -151,29 +181,146 @@ void show(Mat frame)
 	imshow("test", frame);
 }
 
-Mat findWicket(Mat frame)
+/*
+ * function to get binary image of frame
+ * input: 3 channel rgb Mat frame
+ * output: binary Mat image
+ * description:
+ *  - Assumes frame has some zero valued pixels. Applies
+ *    threshold value of 1 to every pixel.
+ *  - Any pixel with value >= 1 is set to 1
+ *  - Zero value pixels (black pixels / rgb(0,0,0)) set to 0
+ */
+Mat getBinary(Mat frame)
+{
+	GpuMat gpuFrame(frame);
+	GpuMat grayFrame;
+	GpuMat binary;
+	int thresh = 1;
+	int maxThresh = 255;
+
+	gpu::cvtColor(gpuFrame, grayFrame, CV_BGR2GRAY);
+	gpu::threshold(grayFrame, binary, thresh, maxThresh, THRESH_BINARY);
+
+	Mat final;
+	binary.download(final);
+
+	gpuFrame.release();
+	grayFrame.release();
+	binary.release();
+
+	return final;
+}
+
+/*
+ * Gaussian blur function
+ * input: 3 channel rgb Mat frame
+ * output: 3 channel rgb Gaussian blurred Mat frame
+ */
+Mat applyGaussian(Mat frame)
+{
+	GpuMat gpuFrame;
+	gpuFrame.upload(frame);
+	GaussianBlur(gpuFrame, gpuFrame, Size(5,5), 2);
+	Mat final;
+	gpuFrame.download(final);
+	gpuFrame.release();
+	return final;
+}
+
+struct boundingBox {
+	Mat frame;
+	float area;
+	int centerX;
+	int centerY;
+	int width;
+	int height;
+};
+
+/*
+ * Function to apply bounding box
+ * input: 3 channel rgb Mat frame (blob image)
+ * output: 3 channel rgb Mat frame with bounding box applied to biggest blob
+ * description:
+ *  - converts original frame into binary
+ *  - applies a bounding box to the largest blob in the frame (contour with largest area)
+ */
+Mat applyBoundingBox(Mat frame)
+{
+	Mat final;
+	frame.copyTo(final);
+	if (frame.channels() < 3) {
+		cerr << "Frame is not 3 channel. Cannot apply bounding box" << endl;
+		return final;
+	}
+
+	//turn color frame into binary for findContours();
+	Mat binary = getBinary(frame);
+
+	//find contours
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+
+	findContours(binary, contours, hierarchy, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
+
+ 	//Finds the contour with the largest area
+	float area = 0;
+	float largest = 0;
+	int idx;
+	for(int i=0; i < contours.size(); i++) {
+		area = contourArea(contours[i], false);
+		if(area > largest) {
+			largest = area;
+			idx = i;
+		}
+	}
+
+	Rect rect;
+	rect = boundingRect(contours[idx]);
+	Point pt1, pt2;
+	pt1.x = rect.x;
+	pt1.y = rect.y;
+	pt2.x = rect.x + rect.width;
+	pt2.y = rect.y + rect.height;
+
+	// Draws the rect in the original image
+	rectangle(final, pt1, pt2, CV_RGB(0,0,255), 1);
+
+	return final;
+}
+
+/*
+ * function containing all transformations used in main infinit loop
+ */
+Mat applyAll(Mat frame)
 {
 	Mat final(frame);
+#ifdef APPLY_GAUSSIAN_BLUR
+	final = applyGaussian(frame);
+#endif
 #ifdef APPLY_HSV_FILTER
 	final = filterOrange(final);
 #endif
-#ifdef APPLY_GAUSSIAN_BLUR
-	GpuMat blur;
-	blur.upload(final);
-	GaussianBlur(blur, blur, Size(5,5), 2);
-	blur.download(final);
-	blur.release();
-	show(final);
+#ifdef APPLY_BOUNDING_BOX
+	final = applyBoundingBox(final);
 #endif
 #ifdef APPLY_CANNY_EDGE
-	final = applyCannyEdge(final);
+ 	final = applyCannyEdge(final);
 #endif
 #ifdef APPLY_HOUGH_LINE
-	final = applyHoughLine(final);
+ 	final = applyHoughLine(final);
 #endif
 	return final;
 }
 
+
+/*
+ * main function
+ * description: original design to take in source from either command line or camera feed.
+ * - If command argument exists, treat it as a video or image file
+ *   - Apply all defined transformations on image or video
+ * - Else, source is camera feed. Infinit loop
+ */
 int main(int argc, char *argv[])
 {
 	if (argc > 1) { //use files from input command as source instead
@@ -186,7 +333,7 @@ int main(int argc, char *argv[])
 			windowName = inputFile;
 			trackbarWindow = inputFile + " trackbar";
 			namedWindow(trackbarWindow.c_str(), WINDOW_NORMAL);
-			cur_frame_applied = findWicket(cur_frame);
+			cur_frame_applied = applyAll(cur_frame);
 		} else {   //try opening as video
 			VideoCapture vid;
 			getVideoFromFile(inputFile.c_str(), vid);
@@ -197,7 +344,7 @@ int main(int argc, char *argv[])
 		exit(0);
 	}
 
-	//using camera as source
+	/************************  using camera as source (below)  *********************/
 	VideoCapture cap(0);
 	if (!cap.isOpened()) {
 		cerr << "Error connecting to a camera device" << endl;
@@ -225,7 +372,7 @@ int main(int argc, char *argv[])
 #ifdef SHOW_RAW
 		imshow("raw", cur_frame);
 #endif
-		cur_frame_applied = findWicket(cur_frame);
+		cur_frame_applied = applyAll(cur_frame);
 		if(!cur_frame_applied.empty()){
 			imshow(windowName, cur_frame_applied);
 		}
